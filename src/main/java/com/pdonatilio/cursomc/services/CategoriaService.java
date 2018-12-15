764592536b8cce47766dbcd804ca12779e2861a4
Paulo Donatilio <pdonatilio@gmail.com>
2018-12-15 18:12:21 +0000

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/pdonatilio/cursomc/services/CategoriaService.java
+++ src/main/java/com/pdonatilio/cursomc/services/CategoriaService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.pdonatilio.cursomc.domain.Categoria;
 import com.pdonatilio.cursomc.repositories.CategoriaRepository;
+import com.pdonatilio.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,6 +15,10 @@ public class CategoriaService {
 	
 	public Categoria find(Integer id) {
 		Categoria obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id:" + id
+					+ ", Tipo: " + Categoria.class.getName());
+		}
 		return obj;
 	}
 
